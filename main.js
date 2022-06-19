3cdd97e28b02777223a25ffbfd0400664e366316
SaurokiFestus <sauroki.festus@student.moringaschool.com>
2022-06-19 12:41:37 +0300

    commit tree 27dea24c6000066cf2b77e4dac44b2296290f2ae

--- main.js
+++ main.js
@@ -3,9 +3,27 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const articleHearts = document.querySelectorAll(".like-glyph");
+function likeCallback(e) {
+  const heart = e.target;
+  mimicServerCall("bogusUrl")
+    .then(function(){
+      if ( heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.className = "activated-heart";
+      } else {
+        heart.innerText = EMPTY_HEART;
+        heart.className = "";
+      }
+    })
+}
 
+for (const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
-
+const modal=document.getElementById("modal");
+modal.classList.add("hidden");
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
